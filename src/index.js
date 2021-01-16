24be3901f90b360643ad5a8421e1ce05f2a8bc4f
Frank Pagan <frank@cocreate.app>
2021-01-16 18:11:40 +0000

    fix: (merge html and commit extractor to one with one config)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const CoCreateExtract = require('./extract')
 const fs = require('fs');
-const { directory, ignores, extensions, socket} = require('../config.json');
+const path = require('path');
+const { directory, ignores, extensions, socket, sources} = require(path.resolve(process.cwd(), 'CoCreate.config.json'));
+
 const {CoCreateSocketInit, CoCreateUpdateDocument, CoCreateCreateDocument } = require("./socket_process.js")
 /**
  * Socket init 
@@ -15,7 +17,7 @@ fs.writeFileSync('result.json', JSON.stringify(result), 'utf8')
 
 
 /**
- * Store data into dab
+ * Store data into db
  */
 result.forEach((docs) => {
 	docs.forEach((doc) => {
@@ -25,5 +27,24 @@ result.forEach((docs) => {
 			CoCreateUpdateDocument(doc, socket.config);
 		}
 	})
+})
+
+/**
+ * update document by config sources
+ */
+sources.forEach(({path, collection, document_id, name, category, ...rest}) => {
+	let content = fs.readFileSync(path, 'utf8');
+	if (content) {
+		CoCreateUpdateDocument({
+			collection,
+			document_id,
+			data: {
+				[name]: content,
+				category,
+				...rest
+			},
+			upsert: true
+		}, socket.config);
+	}
 })
 
