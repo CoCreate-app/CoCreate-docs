b6d13f021e822c28885a4cc17244626cdd52b36b
Frank Pagan <frank@cocreate.app>
2022-12-30 22:30:27 +0000

    feat: define directories to be saved in db. will includece nested files and directoriies

--- src/index.js
+++ src/index.js
@@ -19,107 +19,231 @@ setTimeout(function(){
 	process.exit()
 }, 120000)
 
-const { config, sources, crud  } = CoCreateConfig;
+const { config, directories, sources  } = CoCreateConfig;
 
 CoCreateCrud.socket.create(config)
 config.broadcast = false
 
 
 /**
- * update and create document by config crud
- */
+ * Store files by config directories
+ **/
+let errorLog = [];
 
- if (crud) {
-	crud.forEach(async (data) => {
-		await runStore(data)
-	})
+async function runDirectories() {
+	for (const directory of directories) {
+		const entry = directory.entry
+		const exclude = directory.exclude
+		await runFiles(directory, entry, exclude)
+	}
+	return
 }
 
+async function runFiles(directory, entry, exclude, parentDirectory = '') {
+	let files = fs.readdirSync(entry);
+
+	for (let file of files) {
+		if (exclude && exclude.includes(file)) continue
+
+		let isDirectory = fs.existsSync(`${entry}/${file}`) && fs.lstatSync(`${entry}/${file}`).isDirectory();
+		let name = file
+		let source = ''
+		let directoryName = parentDirectory || '';
+		let mimeType = mime.lookup(`${file}`)
+		let pathName = '';
+
+		if (!directoryName && directory.document && directory.document.directory)
+			directoryName = directory.document.directory.replace('{{directory}}', '').trim()
+		else if (!directoryName)
+			directoryName = '/'
+
+		if (exclude && exclude.includes(directoryName)) continue
+
+		if (directoryName.endsWith("/"))
+			pathName = directoryName + name
+		else if (directoryName)
+			pathName = directoryName + '/' + name
+		else
+			pathName = '/' + name
+
+		if (exclude && exclude.includes(pathName)) continue
+
+		if (isDirectory)
+			mimeType = "text/directory"
+		else
+			source = getSource(`${entry}/${file}`, mimeType)
+
+		let values = {
+			'{{name}}': name,
+			'{{source}}': source,
+			'{{directory}}': directoryName,
+			'{{path}}': pathName,
+			'{{content-type}}': mimeType
+		}
+
+		let document = {...directory.document}
+		if (!document.name)
+			document.name = "{{name}}"
+		if (!document.src)
+			document.src = "{{source}}"
+		if (!document.directory)
+			document.directory = "/{{directory}}"
+		if (!document.path)
+			document.path = "{{path}}"
+		if (!document["content-type"])
+			document["content-type"] = '{{content-type}}'
+		if (!document.public && document.public != false && document.public != 'false')
+			document.public = 'true'
+
+		let object = {
+			collection: directory.collection || 'files',
+			document
+		}
+		for (const key of Object.keys(directory.document)) {
+			if (typeof directory.document[key] == 'string') {
+
+				let variables = directory.document[key].match(/{{([A-Za-z0-9_.,\[\]\-\/ ]*)}}/g);
+				if (variables) {
+					for (let variable of variables) {
+						if (variable == '{{directory}}') {
+							if (parentDirectory)
+								object.document[key] = values[variable]
+							else
+								object.document[key] = object.document[key].replace(variable, '');
+						}
+						else if (isDirectory && variable == '{{source}}')
+							delete object.document[key]
+						else
+							object.document[key] = object.document[key].replace(variable, values[variable]);
+					}
+				}
+				
+			}
+		}
+
+		if (!object.document._id)
+			object.filter = {
+				query: [{name: 'path', value: pathName, operator: '$eq'}]
+			}
+
+		response = await runStore(object);
+		if (response.error)
+			errorLog.push(response.error)
+
+		if (isDirectory && pathName) {
+			let newEntry
+			if (entry.endsWith("/"))
+				newEntry = entry + name
+			else 
+				newEntry = entry + '/' + name
+			
+			await runFiles(directory, newEntry, exclude, pathName)
+		}
+	}
+	if (errorLog.length)
+		console.log(...errorLog)
+
+}
+
+
+function getSource(path, mimeType) {
+let readType = 'utf8'
+if (/^(image|audio|video)\/[-+.\w]+/.test(mimeType))
+	readType = 'base64'
+
+let binary = fs.readFileSync(path);
+let content = new Buffer.from(binary).toString(readType);
+
+return content
+}
 
 /**
- * Store html files by config sources
+ * Store files by config sources
  **/
-if (sources) {
-	let new_sources_list = [];
+async function runSources() {
+	let updatedSources = [];
 
-	async function runSources() {
-		for (let i = 0; i < sources.length; i++) {
-			const { entry, collection, document } = sources[i];
-			
-			let new_source = {...sources[i]};
-			let Key
-			let response = {};
-			if (entry) {
-				try {
-					let read_type = 'utf8'
-					let mime_type = mime.lookup(entry) || 'text/html';
-					if (/^(image|audio|video)\/[-+.\w]+/.test(mime_type)) {
-						read_type = 'base64'
-					}
-					
-					let binary = fs.readFileSync(entry);
-					
-					let content = new Buffer.from(binary).toString(read_type);
+	for (let i = 0; i < sources.length; i++) {
+		const { collection, document } = sources[i];
+		
+		let source = {...sources[i]};
+		let keys = new Map()
+		let response = {};
 
-					if (content && collection) {
-						if (!document) 
-							document = {};
-						else
-							for (const key of Object.keys(document)) {
-								if (document[key] == '{{source}}') {
-									document[key] = content
-									Key = key
-									break;
+		try {
+			if (collection) {
+				if (!document) 
+					document = {};
+				else
+					for (const key of Object.keys(document)) {
+						if (typeof document[key] != 'string') 
+							continue
+
+						let variables = document[key].match(/{{([A-Za-z0-9_.,\[\]\-\/ ]*)}}/g);
+						if (variables) {
+							keys.set(key, `${document[key]}`)
+							let value = ""
+							for (let variable of variables) {
+								let entry = /{{\s*([\w\W]+)\s*}}/g.exec(variable);
+								entry = entry[1].trim()
+								if (entry) {
+									let read_type = 'utf8'
+									let mime_type = mime.lookup(entry) || 'text/html';
+									if (/^(image|audio|video)\/[-+.\w]+/.test(mime_type)) {
+										read_type = 'base64'
+									}
+									
+									let binary = fs.readFileSync(entry);
+									let content = new Buffer.from(binary).toString(read_type);
+									if (content)
+										value += content
+									// document[key] = document[key].replace(variable, content);
 								}
 							}
-
-						response = await runStore({collection, document});
+							document[key] = value
+						}
+				
 					}
-				} catch (err) {
-					console.log(err)
-					process.exit()
-				}
-				if (response.document && response.document[0] && response.document[0]._id) {
-					new_source.document[Key] = '{{source}}'
-					new_source.document._id = response.document[0]._id
-				} else {
-					console.log('_id could not be found')
-					process.exit()
-				}
+
+				let data = {collection, document}
+				if (!document._id && document.path)
+					data.filter = {
+						query: [{name: 'path', value: document.path, operator: '$eq'}]
+					}
+
+				response = await runStore(data);
 			}
-			new_sources_list.push(new_source)
-		}
-
-		return new_sources_list
-	}
-	
-	runSources().then(() => {
-		let new_config = {
-			config,
-			sources: new_sources_list,
-			crud: crud,
-		}
-		delete new_config.config.broadcast
-		let write_str = JSON.stringify(new_config, null, 4)
-		write_str = "module.exports = " + write_str;
-
-		fs.writeFileSync(configFile, write_str);
-		setTimeout(function(){
+		} catch (err) {
+			console.log(err)
 			process.exit()
-		}, 2000)		
-	})
+		}
+		if (response.document && response.document[0] && response.document[0]._id) {
+			for (const [key, value] of keys) {
+				source.document[key] = value
+			}
+			source.document._id = response.document[0]._id
+		} else {
+			console.log('_id could not be found')
+			process.exit()
+		}
+
+		updatedSources.push(source)
+	}
+
+	return updatedSources
 }
+	
 
 async function runStore(data) {
 	try {
 		let response;
-		if (!data.document._id) {
+		if (!data.document._id && !data.filter) {
 			response = await CoCreateCrud.createDocument({
 				...config,
 				...data
 			})
 		} else {
-			response = await  CoCreateCrud.updateDocument({
+			response = await CoCreateCrud.updateDocument({
 				...config,
 				...data,
 				upsert: true
@@ -133,3 +257,28 @@ async function runStore(data) {
 		return null;
 	}
 } 
+
+async function run() {
+	if (directories)
+		await runDirectories()
+	
+	if (sources) {
+		let sources = await runSources()
+		let new_config = {
+			config,
+			directories,
+			sources,
+		}
+		delete new_config.config.broadcast
+		let write_str = JSON.stringify(new_config, null, 4)
+		write_str = "module.exports = " + write_str;
+	
+		fs.writeFileSync(configFile, write_str);	
+	}
+
+	setTimeout(function(){
+		process.exit()
+	}, 2000)		
+}
+
+run()
